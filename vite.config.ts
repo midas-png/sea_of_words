b70aeffe7dcb21c471fc683d643d2fd099f44248
midas-png <y4nlvn@gmail.com>
2024-10-14 12:28:50 +0300

    fix: build folder deploy changed to dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/sea_of_words/',
+    base: "/sea_of_words/",
+    build: {
+        outDir: "dist",
+    },
     resolve: {
         alias: [
             {
